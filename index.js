1f03a9888d62e355f83111fdea6bc4b03230bc0b
student2020heroku <72618352+student2020heroku@users.noreply.github.com>
2020-11-22 10:19:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import m from 'mongoose';
+import mstore from 'connect-mongo';
 import dot from 'dotenv';
+import session from 'express-session';
 import fs from 'fs';
 import appSrc from './app.js';
 import CORS from './CORS.js';
@@ -11,11 +13,11 @@ import UserController from './routes/UserController.js';
 dot.config({ path: './.env' });
 const { URL } = process.env;
 const User = UserModel(m);
-const app = appSrc(express, bodyParser, fs, CORS, User, UserController);
+const app = appSrc(express, bodyParser, fs, CORS, m, mstore, session);
 
 try {
     await m.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true });
-    app.listen(process.env.PORT ?? 4321);
+    app.listen(process.env.PORT ?? 3000);
 } catch(e) {
     console.log(e.codeName);
 }
