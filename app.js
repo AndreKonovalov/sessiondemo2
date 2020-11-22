7f52975df2dae1cb7bf66815a77b5fff4e018f5c
student2020heroku <72618352+student2020heroku@users.noreply.github.com>
2020-11-22 10:19:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
-export default (express, bodyParser, fs, CORS, User, UserController) => {
+export default (express, bodyParser, fs, CORS, m, mstore, session) => {
     const app = express();
-    
+    const db = m.connection;
+    const MongoStore = mstore(session);
+    const protect = (r, res, next) => {
+        if (r.session.name === 'admin') return next();
+        res.redirect('/denied');
+    };
     app
     .use((r, res, next) => r.res.set(CORS) && next())
     .use(bodyParser.urlencoded({ extended: true }))
-    .use('/user', UserController(express, User))
+    .use(session({ 
+        secret: 'mysecret', 
+        resave: true, 
+        saveUninitialized: true,
+        store: new MongoStore({ mongooseConnection: db }) 
+    }))
+  
     .get('/login/', (req, res) => res.send('eliasgoss'))   
     .get('/code/', (req, res) => fs.createReadStream(import.meta.url.substring(7)).pipe(res))
+    .get('/denied', r => r.res.status(403).send('Доступ запрещён'))
+    .get('/prune', r => {
+        delete r.session.name;
+        r.res.send(`Очищено!`);  
+    })
+    .get('/profile', protect, r => {
+        const { name } = r.session;
+        r.res.send(`Доступ открыт, ${name}!`);
+    })
+    .get('/set/:user', r => {
+        const { user } = r.params;
+        r.session = r.session || {};
+        r.session.name = user;
+        r.res.send(`Установлено: ${user}!`); 
+    })
     .all('/*', r => r.res.send('Работает!'));
    
    
