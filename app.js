0b88bd4175ed4f8d482f754765060041b66eac9d
AndreKonovalov <47122488+AndreKonovalov@users.noreply.github.com>
2020-12-06 13:02:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -11,14 +11,17 @@ const htxt = { 'Content-Type': 'text/plain; charset=utf-8' , ...CORS };
 export default (express, bodyParser, fs) => {
     const app = express();
     app
-    .use((r, res, next) => r.res.set(htxt) && next())
+    .use((r, res, next) => {
+      log('htxt ' + htxt +'\n');
+      r.res.set(htxt) && next();
+    })
     .use(bodyParser.urlencoded({ extended: true }))
   
     .get('/login/', (req, res) => res.send('eliasgoss'))   
     .get('/code/', (req, res) => fs.createReadStream(import.meta.url.substring(7)).pipe(res))
     .get('/denied', r => r.res.status(403).send('Доступ запрещён'))
     .get('/prune', r => {
-        let str1 = 'r ' + JSON.stringify(r) +'\n';
+        let str1 = 'str1\n'; // 'r ' + JSON.stringify(r) +'\n';
         let str2 = 'res ' + JSON.stringify(r.res) +'\n';
         log(str1 + str2);
 //        delete r.session.name;
