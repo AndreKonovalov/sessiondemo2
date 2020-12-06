3c85415c0025c5a11dd898832c02173b0a538fa6
AndreKonovalov <47122488+AndreKonovalov@users.noreply.github.com>
2020-12-06 12:39:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import m from 'mongoose';
-import mstore from 'connect-mongo';
-import dot from 'dotenv';
-import session from 'express-session';
+// import m from 'mongoose';
+// import mstore from 'connect-mongo';
+// import dot from 'dotenv';
+// import session from 'express-session';
 import fs from 'fs';
 import appSrc from './app.js';
-import CORS from './CORS.js';
-import UserModel from './models/User.js';
-import UserController from './routes/UserController.js';
+//import UserModel from './models/User.js';
+//import UserController from './routes/UserController.js';
 
-dot.config({ path: './.env' });
-const { URL } = process.env;
-const User = UserModel(m);
-const app = appSrc(express, bodyParser, fs, CORS, m, mstore, session);
+// dot.config({ path: './.env' });
+// const { URL } = process.env;
+// const User = UserModel(m);
+const app = appSrc(express, bodyParser, fs);
 
 try {
-    await m.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true });
+//    await m.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true });
     app.listen(process.env.PORT ?? 3000);
 } catch(e) {
     console.log(e.codeName);
