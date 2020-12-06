4085c3e7c9e1e05250a8fa1ec9e0899653345cd4
AndreKonovalov <47122488+AndreKonovalov@users.noreply.github.com>
2020-12-06 17:08:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-// import m from 'mongoose';
-// import mstore from 'connect-mongo';
-// import dot from 'dotenv';
-// import session from 'express-session';
+import m from 'mongoose';
+import mstore from 'connect-mongo';
+//import dot from 'dotenv';
+import session from 'express-session';
 import fs from 'fs';
 import appSrc from './app.js';
-//import UserModel from './models/User.js';
-//import UserController from './routes/UserController.js';
+import UserModel from './models/User.js';
+import UserController from './routes/UserController.js';
 
 // dot.config({ path: './.env' });
 // const { URL } = process.env;
-// const User = UserModel(m);
-const app = appSrc(express, bodyParser, fs);
+URL = 'mongodb+srv://readwriter:Fagku3Kuf4@cluster0.a6m0m.mongodb.net/mongodemo?retryWrites=true&w=majority';
+const User = UserModel(m);
+const app = appSrc(express, bodyParser, fs, m, mstore, session);
 
 try {
-//    await m.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true });
+    await m.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true });
     app.listen(process.env.PORT ?? 3000);
 } catch(e) {
     console.log(e.codeName);
