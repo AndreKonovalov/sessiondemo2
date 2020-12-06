c18f2280edfd0299f5a4c19df0a4803ddfb428e9
AndreKonovalov <47122488+AndreKonovalov@users.noreply.github.com>
2020-12-06 17:03:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,11 +8,23 @@ const CORS = {
 const hhtml = { 'Content-Type': 'text/html; charset=utf-8' , ...CORS };
 const htxt = { 'Content-Type': 'text/plain; charset=utf-8' , ...CORS };
 
-export default (express, bodyParser, fs) => {
+export default (express, bodyParser, fs, m, mstore, session) => {
     const app = express();
+    const db = m.connection;
+    const MongoStore = mstore(session);
+    const protect = (r, res, next) => {
+        if (r.session.name === 'admin') return next();
+        res.redirect('/denied');
+    };
     app
     .use( (r, res, next) => res.set(htxt) && next() )
     .use(bodyParser.urlencoded({ extended: true }))
+    .use(session({ 
+        secret: 'mysecret', 
+        resave: true, 
+        saveUninitialized: true,
+        store: new MongoStore({ mongooseConnection: db }) 
+    }))
   
     .get('/login/', (req, res) => res.send('eliasgoss'))   
     .get('/code/', (req, res) => fs.createReadStream(import.meta.url.substring(7)).pipe(res))
@@ -23,19 +35,19 @@ export default (express, bodyParser, fs) => {
         let str2 = 'res ' + Object.keys(r.res) +'\n';
         let str3 = 'req ' + Object.keys(r.res.req) +'\n';
         log(str1 + str2 + str3);
-//        delete r.session.name;
+        delete r.session.name;
         r.res.send(`Очищено!\n` + str1 + str2 + str3);
     })
-//     .get('/profile', protect, r => {
-//         const { name } = r.session;
-//         r.res.send(`Доступ открыт, ${name}!`);
-//     })
-//     .get('/set/:user', r => {
-//         const { user } = r.params;
-//         r.session = r.session || {};
-//         r.session.name = user;
-//         r.res.send(`Установлено: ${user}!`); 
-//     })
+     .get('/profile', protect, r => {
+         const { name } = r.session;
+         r.res.send(`Доступ открыт, ${name}!`);
+     })
+     .get('/set/:user', r => {
+         const { user } = r.params;
+         r.session = r.session || {};
+         r.session.name = user;
+         r.res.send(`Установлено: ${user}!`); 
+     })
     .all('/*', r => r.res.send('Работает!'));
    
     return app;
